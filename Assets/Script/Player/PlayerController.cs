fbc5e264d719f32bf3726f282e826deae4fa9b9d
RootlessDuckweed <1064371974@qq.com>
2024-03-21 23:21:11 +0800

    新增主角二段攻击和滑铲

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -8,16 +8,34 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb; //自身的刚体组件
     private PlayerInput input; //输入控制器
     public Vector2 inputDirection; //输入人物移动的方向
+    public PlayerAnimaton playerAnimaton; //自身的PlayerAnimation脚本组件
+    public PhysicsCheck physicsCheck; //自身物理检测脚本组件
+  
     public float moveSpeed;
     public float currentFace;
     public float jumpForce;
+    public float slideForce;
+    public float slideTimeDuration;
+    public float slideCounter;
+    public bool isSlideCold;  //是否在滑铲冷却状态
+    public bool isAttack;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimaton = GetComponent<PlayerAnimaton>();
+        physicsCheck = GetComponent<PhysicsCheck>();
+        
         input = new PlayerInput();
         input.GamePlay.Jump.started += Jump;
+        input.GamePlay.Slide.performed += Slide;
+        input.GamePlay.Attack.started += PlayerAttack;
     }
 
+    private void PlayerAttack(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        playerAnimaton.TriggerAttack();
+        isAttack = true;
+    }
 
     private void OnEnable()
     {
@@ -31,6 +49,7 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
+        SlideTimeCounter();
         inputDirection = input.GamePlay.Move.ReadValue<Vector2>();
     }
 
@@ -48,11 +67,42 @@ public class PlayerController : MonoBehaviour
         transform.localScale = new Vector3(currentFace, transform.localScale.y, transform.localScale.z);
         //移动
         rb.velocity = new Vector2(moveSpeed * inputDirection.x* Time.deltaTime, rb.velocity.y);
+        if (isAttack)
+        {
+            rb.velocity = new Vector2(0,rb.velocity.y);
+        }
     }
 
     private void Jump(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
+        if(physicsCheck.isGround)
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
+    private void Slide(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if (isSlideCold) return;
+        if (physicsCheck.isGround)
+        {
+            playerAnimaton.TriggerSlide();
+            moveSpeed *= 1.5f;
+            slideCounter = slideTimeDuration;
+            isSlideCold = true;
 
+        }
+        
+    }
+    void SlideTimeCounter()
+    {
+        if (!isSlideCold) return;
+        else
+        {
+            slideCounter -= Time.deltaTime;
+            if(slideCounter <= 0f){
+                isSlideCold = false;
+            }
+        }
+    }
+
+   
+    
 }
