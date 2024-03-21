c7b44072e9c855698e90d61cd0977cc49c6621c6
RootlessDuckweed <1064371974@qq.com>
2024-03-21 16:29:19 +0800

    Initial commit

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Character : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    public float invulnerableDuration; //受伤无敌的时长间隔
+    public float invulnerableCounter; //受伤无敌秒数 计数
+    public bool invulnerable;
+
+    public UnityEvent<Character> OnHealthChange; //血量发生变化通知订阅者
+    public UnityEvent<Transform> OnTakenDamage; // 受伤时候 通知订阅者
+    public UnityEvent OnDead; // 死亡的时候通知订阅者
+
+    private void Update()
+    {
+        //如果触发无敌时间，那么就计时无敌的时间 
+        InvulnerableConter();
+    }
+
+    /// <summary>
+    /// 当进入到一些碰撞体时处理相关逻辑
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+
+    /// <summary>
+    /// 角色受到伤害 逻辑
+    /// </summary>
+    /// <param name="attacker"></param>
+    public void TakeDamage(Attack attacker)
+    {
+        if (invulnerable)
+        {
+            return;
+        }
+        if (currentHealth - attacker.damage > 0f)
+        {
+            currentHealth -= attacker.damage;
+            TriggerInvulnerable();
+            //执行受伤
+            OnTakenDamage?.Invoke(attacker.transform); //通知订阅者，受伤了
+        }
+        else
+        { 
+            //触发死亡 通知订阅角色死亡的订阅者
+            if (currentHealth > 0) OnDead?.Invoke();
+            currentHealth = 0f;
+
+
+        }
+        OnHealthChange?.Invoke(this);//执行血量变化操作 通知订阅者
+    }
+
+    /// <summary>
+    /// 触发受伤之后无敌一段时间
+    /// </summary>
+    void TriggerInvulnerable()
+    {
+        if (!invulnerable)
+        {
+            invulnerable = true;
+            invulnerableCounter = invulnerableDuration;
+        }
+    }
+
+    /// <summary>
+    /// 如果触发了无敌时间，那么就计时无敌的时间 ，无敌时间一过就重置无敌
+    /// </summary>
+    void InvulnerableConter()
+    {
+        if (invulnerable)
+        {
+            invulnerableCounter -= Time.deltaTime;
+            if (invulnerableCounter <= 0f)
+            {
+                invulnerable = false;
+            }
+        }
+    }
+
+}
+
---
+++ Assets/Script/PhysicsCheck.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsCheck : MonoBehaviour
+{
+    public float checkRadius; //¼ì²â°ë¾¶
+    public LayerMask layer;  //¼ì²âµÄ²ã¼¶
+    [Header("Check Ground")]
+    public Vector2 checkPointOffset_Ground; //Æ«ÒÆ
+    public bool isGround; //ÊÇ·ñÔÚµØÃæ
+    [Header("Check Air")]
+    public Vector2 checkPointOffset_Air;
+    public bool isAir; // ÊÇ·ñÔÚ¿ÕÖÐ
+    [Header("Check Left Wall")]
+    public Vector2 checkPointOffset_LeftWall; //Æ«ÒÆ
+    public bool isLeftWall; //ÊÇ·ñ¿¿½ü×ó±ßÇ½Ìå
+    [Header("Check Right Wall")]
+    public Vector2 checkPointOffset_RightWall; //Æ«ÒÆ
+    public bool isRightWall; // ÊÇ·ñ¿¿½üÓÒ±ßÇ½Ìå
+
+    private void Awake()
+    {
+        
+    }
+    void Update()
+    {
+        Check();
+    }
+
+   
+    /// <summary>
+    /// ¼ì²â
+    /// </summary>
+    void Check()
+    {
+        isGround = Physics2D.OverlapCircle((Vector2)transform.position + checkPointOffset_Ground * transform.localScale, checkRadius, layer);
+        isRightWall = Physics2D.OverlapCircle((Vector2)transform.position + checkPointOffset_RightWall, checkRadius, layer);
+        isLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + checkPointOffset_LeftWall, checkRadius, layer);
+        isAir = !Physics2D.OverlapCircle((Vector2)transform.position + checkPointOffset_Air, checkRadius, layer);
+    }
+    /// <summary>
+    /// »­³ö¼ì²â·¶Î§
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere((Vector2)transform.position + checkPointOffset_Ground * transform.localScale, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + checkPointOffset_LeftWall, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + checkPointOffset_RightWall, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + checkPointOffset_Air, checkRadius);
+    }
+}
