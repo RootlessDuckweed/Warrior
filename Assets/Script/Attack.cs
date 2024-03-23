394437b56a8a4316457f5318266f14cdd13c6f7d
RootlessDuckweed <1064371974@qq.com>
2024-03-23 23:19:23 +0800

    修复了敌人和玩家交互的bug

--- Assets/Script/Attack.cs
+++ Assets/Script/Attack.cs
@@ -13,5 +13,6 @@ public class Attack : MonoBehaviour
     private void OnTriggerStay2D(Collider2D other)
     {
         other.GetComponent<Character>()?.TakeDamage(this);
+        print(other.gameObject.name);
     }
 }
