46cd1d35a0d51e639804229cdd512748df4ca054
KenshinZhan <131251780+KenshinZhan@users.noreply.github.com>
2024-04-09 00:08:14 +0800

    实现了背包的基本功能
    背包的打开,添加物品到背包,使用物品之后移除背包,里面有一些测试使用的类或对象,可以移除

--- Assets/Script/ScriptableObject/Inventory/InventorySO.cs
+++ Assets/Script/ScriptableObject/Inventory/InventorySO.cs
@@ -1,44 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 //背包
+[Serializable]
 [CreateAssetMenu(menuName = "Inventory/InventorySO")]
 public class InventorySO : ScriptableObject
 {
-    public Dictionary<string,PropStack> propsPakage=new Dictionary<string, PropStack>();
+    public Dictionary<string,int> propsPakage=new Dictionary<string, int>();
     public void AddProp(PropSO propSO,int count)
     {
-        if(propsPakage.ContainsKey(propSO.name))
+        if(propsPakage.ContainsKey(propSO.propName))
         {
             //若里面背包中本来就有该道具,则直接在字典集中为道具对应的PropStack增加数量
-            propsPakage[propSO.name].propNum += count;
+            propsPakage[propSO.propName] += count;
         }
         //如果字典集中没有这个实际存储的道具内容,就在字典集中加入
         else
         {
-            PropStack propStack = new PropStack();
-            propStack.prop = propSO;
-            propStack.propNum = count;
-            propsPakage.Add(propSO.name, propStack);
-
+            propsPakage.Add(propSO.propName,count);
         }
     }
     //移除一个背包内的道具
     public void RemoveProp(PropSO propSO) {
         //如果有该道具则该道具在背包内的数量减少,否则直接返回
-        if(propsPakage.ContainsKey(propSO.name))
+        if(propsPakage.ContainsKey(propSO.propName))
         {
-            propsPakage[propSO.name].propNum--;
+            propsPakage[propSO.propName]--;
         }
         else
         {
             return;
         }
         //当该背包中的该类道具数量减少为0时,将该道具移除
-        if (propsPakage[propSO.name].propNum==0)
+        if (propsPakage[propSO.propName] ==0)
         {
-            propsPakage.Remove(propSO.name);
+            propsPakage.Remove(propSO.propName);
         }
     }
 }
---
+++ Assets/Script/UI/Panel/SlotDescriptionPanel.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+
+public class SlotDescriptionPanel : BasePanel
+{
+    public PropSO propSO;
+    public TMP_Text titleText;
+    public TMP_Text descriptionText;
+    Button useButton;
+    Button cancelButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        useButton=transform.Find("UseButton").GetComponent<Button>();
+        cancelButton=transform.Find("CancelButton").GetComponent<Button>();
+        useButton.onClick.AddListener(OnUseButtonClicked);
+        cancelButton.onClick.AddListener(OnCancelButtonClicked);
+    }
+
+    //Bocchi:点击使用按钮调用的事件
+    void OnUseButtonClicked()
+    {
+        propSO.UseProp();
+        UIManager.Instance.ClosePanel(gameObject.name);
+    }
+
+    //Bocchi:点击取消按钮调用的事件
+    void OnCancelButtonClicked()
+    {
+        UIManager.Instance.ClosePanel(gameObject.name);
+    }
+}
