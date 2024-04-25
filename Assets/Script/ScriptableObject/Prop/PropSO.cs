063c85d5f332036520440b3e166d7aeb230b32a6
KenshinZhan <131251780+KenshinZhan@users.noreply.github.com>
2024-04-25 20:39:30 +0800

    增加药水的图片以及其SO,并将SO中的物体描述的显示改为TextArea

--- Assets/Script/ScriptableObject/Prop/PropSO.cs
+++ Assets/Script/ScriptableObject/Prop/PropSO.cs
@@ -10,6 +10,7 @@ using System;
 public class PropSO : ScriptableObject,IProp
 {
     public string propName; //名字
+    [TextArea]
     public string description; //描述
     public Sprite Image; //显示的图片
     public UnityEvent usePropEvent; //使用了道具之后，我们会触发什么事件
